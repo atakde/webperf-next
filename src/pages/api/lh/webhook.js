f353dc972d22b614b91c36e1e8583e861d4d9737
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2024-10-16 07:55:20 +0300

    chore(lh): debug

--- src/pages/api/lh/webhook.js
+++ src/pages/api/lh/webhook.js
@@ -1,10 +1,20 @@
 import { createClient } from "@/utils/supabase/server";
 
 export default async function handler(req, res) {
-  const supabase = createClient({ req, res });
-  const { urlId } = req.query;
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+  const { urlId, url, deviceType, output } = req.body;
 
-  res.status(200).json({ name: urlId });
+  if (!urlId && !url && !deviceType && !output) {
+    res.status(400).json({ message: "Invalid request" });
+  }
 
-  const { data, error } = await supabase.from("lh").select();
+  // const supabase = createClient({ req, res });
+
+  console.log(urlId, url, deviceType, output);
+
+  res.status(200).json({ message: "Debugging" });
+
+  // const { data, error } = await supabase.from("lh").select();
 }
