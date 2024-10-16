ac4255f3a3d7172246b09fb9b4f669373375b6cf
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2024-10-16 07:46:09 +0300

    chore(lh): debug

--- src/pages/api/lh/webhook.js
+++ src/pages/api/lh/webhook.js
@@ -1,10 +1,10 @@
 import { createClient } from "@/utils/supabase/server";
 
-const endpoints = JSON.parse(process.env.ENDPOINTS ?? '{}');
-
 export default async function handler(req, res) {
   const supabase = createClient({ req, res });
   const { urlId } = req.query;
+
+  res.status(200).json({ name: urlId });
 
   const { data, error } = await supabase.from("lh").select();
 }
